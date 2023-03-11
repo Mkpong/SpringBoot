c153594b9592780e3c4cc1dc1f129aae83a7c8b4
Mkpong <leeja042499@gmail.com>
2023-03-11 16:18:02 +0900

    study03 start

---
+++ springboot_study_03/src/main/java/com/example/springboot_study_03/SpringbootStudy03Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot_study_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudy03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStudy03Application.class, args);
+    }
+
+}
