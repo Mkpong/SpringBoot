f0763581ffda4e953cc8e1fc5efef34afca01ac3
Mkpong <leeja042499@gmail.com>
2023-03-11 16:25:22 +0900

    study03 restart

--- springboot_study_03/src/main/java/com/example/springboot_study_03/SpringbootStudy03Application.java
+++ springboot_study_03/src/main/java/com/example/springboot_study_03/SpringbootStudy03Application.java
@@ -1,13 +1,13 @@
-package com.example.spring_study_03;
+package com.example.springboot_study_03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringStudy03Application {
+public class SpringbootStudy03Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringStudy03Application.class, args);
+        SpringApplication.run(SpringbootStudy03Application.class, args);
     }
 
 }
